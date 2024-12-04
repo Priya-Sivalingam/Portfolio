b0ee79dda9e8cf7ace74b645314e802a3db7eea8
Priya-Sivalingam <priyasivalingam99@gmail.com>
2024-12-04 13:22:20 +0530

    updates whatsapp image

--- src/components/header/contact.jsx
+++ src/components/header/contact.jsx
@@ -1,12 +1,18 @@
 // src/components/WhatsAppIcon.js
 import React from "react";
 import "./header.css"; // We'll create this CSS file next
+import WhatsAppLogo from "../../assets/logo-whatsapp-png-46041.png";
 
 const WhatsAppIcon = () => {
   return (
     <div className="whatsapp-icon">
       <a href="https://wa.me/+94771781833" target="_blank" rel="noopener noreferrer">
-      <img width="50" height="50" src="https://img.icons8.com/external-those-icons-flat-those-icons/50/external-WhatsApp-Logo-social-media-those-icons-flat-those-icons.png" alt="external-WhatsApp-Logo-social-media-those-icons-flat-those-icons"/>
+      <img 
+          width="48" 
+          height="48" 
+          src={WhatsAppLogo} 
+          alt="WhatsApp" 
+        />
       </a>
     </div>
   );
