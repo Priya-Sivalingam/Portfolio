2d91c5dca2a5a736784522dc128a9be7b84dc10a
Priya-Sivalingam <priyasivalingam99@gmail.com>
2024-12-09 17:15:13 +0530

    Updates

---
+++ precache-manifest.8b00febe6e1828043b5e2cb10319d3c8.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9ec5da90d9cd32c1997800d642ddb7",
+    "url": "/Priya-Sivalingam/Portfolio/index.html"
+  },
+  {
+    "revision": "0b612b89f0bd4c870f24",
+    "url": "/Priya-Sivalingam/Portfolio/static/css/main.5196df0b.chunk.css"
+  },
+  {
+    "revision": "b11b3f8689ed2314046b",
+    "url": "/Priya-Sivalingam/Portfolio/static/js/2.6ea405d4.chunk.js"
+  },
+  {
+    "revision": "0b612b89f0bd4c870f24",
+    "url": "/Priya-Sivalingam/Portfolio/static/js/main.29db7840.chunk.js"
+  },
+  {
+    "revision": "754f2b0a3d77d1208df4",
+    "url": "/Priya-Sivalingam/Portfolio/static/js/runtime~main.d5aead85.js"
+  },
+  {
+    "revision": "2f23529689f7ecbfb415505c6d70828c",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/Profile.2f235296.jpg"
+  },
+  {
+    "revision": "af5afbbc38c9c732af83a7442cfc7de0",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/Resume_Manimohan_T_ (2).af5afbbc.pdf"
+  },
+  {
+    "revision": "96d49fa98516cfdae36a925ff0c30588",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/logo-whatsapp-png-46041.96d49fa9.png"
+  },
+  {
+    "revision": "67a053e410b235037f3e0c625c4c4c68",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project1.67a053e4.jpg"
+  },
+  {
+    "revision": "b341776af389bdd90a50b8031fe90398",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project12.b341776a.jpg"
+  },
+  {
+    "revision": "d95c175a2f608636c992e456d92569a5",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project13.d95c175a.jpg"
+  },
+  {
+    "revision": "c08d55efcb469b911f61a9e7978141fa",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project14.c08d55ef.jpg"
+  },
+  {
+    "revision": "4e04732e7e99120276dcc69eff99bc2f",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project15.4e04732e.jpg"
+  },
+  {
+    "revision": "252ab7988a400ed6a68cc819b82a7eb5",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project19.252ab798.jpg"
+  },
+  {
+    "revision": "1950d50b54d79b7fba0df21050f10a37",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project2.1950d50b.jpg"
+  },
+  {
+    "revision": "8f7fd0ffbe98f1660fd12475f0a12380",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project20.8f7fd0ff.jpg"
+  },
+  {
+    "revision": "aa39e7c12fac7e639aa64670d49804c1",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project21.aa39e7c1.jpg"
+  },
+  {
+    "revision": "70b2aaa6b6b7aeb1357484921be07376",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project22.70b2aaa6.jpg"
+  },
+  {
+    "revision": "5dce9846cd32a228063bcf6c4fbde0ce",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project23.5dce9846.jpg"
+  },
+  {
+    "revision": "46074855ffa19dd382d1e506374986bc",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project24.46074855.jpg"
+  },
+  {
+    "revision": "c8e95dec985503a763ff642139b4bb37",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project3.c8e95dec.jpg"
+  },
+  {
+    "revision": "077a24a50b841462b90583fcc0c1a80b",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project4.077a24a5.jpg"
+  },
+  {
+    "revision": "6c0895a4bdfa7a41d5e1b2adc5641cc5",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project5.6c0895a4.jpg"
+  },
+  {
+    "revision": "4eb1d82d3dda63cceff64debaa9ca706",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project6.4eb1d82d.jpg"
+  },
+  {
+    "revision": "84ee46e5b3e25af369263ea8b7791fcc",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project7.84ee46e5.jpg"
+  },
+  {
+    "revision": "21bc69943a8c3035fd29eaae5339fe68",
+    "url": "/Priya-Sivalingam/Portfolio/static/media/project8.21bc6994.jpg"
+  }
+]);
